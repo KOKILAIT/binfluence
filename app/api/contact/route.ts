e3a96ff9fb65f363a8fba56150c8c59193b0b98a
techtutortamil <techtutortamil@gmail.com>
2024-05-01 21:29:02 +1000

    move the config

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,22 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
+import { sanitizeInput } from "../../../components/Sanitiser";
 import { sendMail } from "../../../helper/mailer";
-import { sanitizeInput } from '../../../components/Sanitiser';
 
 export async function POST(request: NextRequest) {
   try {
-    const reqBody = await request.json()
-    const { name, email, message } = reqBody
+    const reqBody = await request.json();
+    const { name, email, message } = reqBody;
 
-    const mailbody = await sendMail(sanitizeInput(name), sanitizeInput(email), sanitizeInput(message))
+    const mailbody = await sendMail(
+      sanitizeInput(name),
+      sanitizeInput(email),
+      sanitizeInput(message)
+    );
     console.log("response in route", mailbody);
     return NextResponse.json({
-      mailbody
-
-    })
+      mailbody,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
-  catch (error: any) {
-    return NextResponse.json({ error: error.message },
-      { status: 500 })
-  }
-
 }
--- helper/mailer.js
+++ helper/mailer.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
-require("dotenv").config();
 
 const CLIENT_ID = " ";
 const CLIENT_SECRET = " ";
